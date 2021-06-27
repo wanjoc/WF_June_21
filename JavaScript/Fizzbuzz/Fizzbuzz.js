4ea4ed5df0076ae6bfd417c5dbd28f15a05c696e
Cynthia Wambui <wanjoc@uw.edu>
2021-06-26 22:49:47 -0700

    loops and conditionals practice

--- JavaScript/Fizzbuzz/Fizzbuzz.js
+++ JavaScript/Fizzbuzz/Fizzbuzz.js
@@ -1,3 +1,12 @@
+/* 
+Write code that will go through each number from 1 to 100 and:
+
+For each number that is a multiple of 3, print "Fizz"
+For each number that is a multiple of 5, print "Buzz"
+For numbers which are a multiple of both 3 and 5, print "FizzBuzz"
+All other numbers should just print normally
+*/
+
 for (var i=1; i<=100; i++){
     if ((i%3== 0) && (i%5 == 0)){
         console.log("FizzBuzz");
